98ae7bfb5800862c7cc0f0da9f61f34ff5db2118
mazafaka2026 <mazafaka2026@bk.ru>
2016-02-29 02:31:19 +0600

    Update test.py

--- test.py
+++ test.py
@@ -2,45 +2,49 @@ print "helo"
 import socket, threading
 
 def ClientTr():
-	print "test 1"
-	while True:
-		data=conn.recv(1024)
-		print(data)
-		if not data:
-			break
-		else:
-			if data == "close":
-				conn.close()
-			else:
-				conn.send(data)
-				conn.close()
-		
+    print "test 1"
+    while True:
+        data = conn.recv(1024)
+
+        if not data:
+            break
+        else:
+            print "data"
+            print(data)
+			print"s"
+			print (s)
+            if data == "close":
+                conn.close()
+            else:
+                conn.send(data)
+                conn.close()
+
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('0.0.0.0', 2222))
 s.listen(1)
 while True:
-	conn, addr = s.accept()	
-	if True:
-		threading.Thread(target=ClientTr).start()
-#threading.Thread(target=ClientTr).start()
-#threading.Thread(target=ClientTr).start()
-#threading.Thread(target=ClientTr).start()
-##threading.Thread(target=ClientTr).start()
-#threading.Thread(target=ClientTr).start()
-#threading.Thread(target=ClientTr).start()
-#threading.Thread(target=ClientTr).start()
-#threading.Thread(target=ClientTr).start()
-#threading.Thread(target=ClientTr).start()
-# while True:
-	# conn, addr = s.accept()
-	# while True:
-	 	# data=conn.recv(1024)
-	 	# if not data:
-			# break
-		# else:
-			# if data == "close":
-				# conn.close()
-			# else:
-				# conn.send(data)
-# conn.close()  
-
+    conn, addr = s.accept()
+    if True:
+        threading.Thread(target=ClientTr).start()
+        # threading.Thread(target=ClientTr).start()
+        # threading.Thread(target=ClientTr).start()
+        # threading.Thread(target=ClientTr).start()
+        ##threading.Thread(target=ClientTr).start()
+        # threading.Thread(target=ClientTr).start()
+        # threading.Thread(target=ClientTr).start()
+        # threading.Thread(target=ClientTr).start()
+        # threading.Thread(target=ClientTr).start()
+        # threading.Thread(target=ClientTr).start()
+        # while True:
+        # conn, addr = s.accept()
+        # while True:
+        # data=conn.recv(1024)
+        # if not data:
+        # break
+        # else:
+        # if data == "close":
+        # conn.close()
+        # else:
+        # conn.send(data)
+        # conn.close()
