c5882c38bf7484e6b896aa14046d8bce7b630c35
mazafaka2026 <mazafaka2026@bk.ru>
2016-02-29 09:03:28 +0600

    Update test.py

--- test.py
+++ test.py
@@ -28,7 +28,7 @@ s.bind(('0.0.0.0', 2222))
 s.listen(10)
 while True:
     conn, addr = s.accept()
-    if x in xrange(10):
+    if i in xrange(10):
         potok().start()
         # threading.Thread(target=ClientTr).start()
         # threading.Thread(target=ClientTr).start()
