1b5b16e23e1adbcd2e5d447ac33e4cb833fcd3cc
mazafaka2026 <mazafaka2026@bk.ru>
2016-02-29 10:17:48 +0600

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,8 @@ import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('0.0.0.0', 2222))
 s.listen(10)
-while True:
+print ("s="+ (s) )
+#while True:
 	conn, addr = s.accept()
 	while True:
 	 	data=conn.recv(1024)
