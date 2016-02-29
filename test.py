cf36bc5e5c7910688a579efdf42fa3af4bbc3361
mazafaka2026 <mazafaka2026@bk.ru>
2016-02-29 10:14:20 +0600

    Update test.py

--- test.py
+++ test.py
@@ -3,6 +3,15 @@ import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('0.0.0.0', 2222))
 s.listen(10)
-conn, addr = s.accept()
-print ("s")
-print (conn+addr)
+while True:
+	conn, addr = s.accept()
+	while True:
+	 	data=conn.recv(1024)
+	 	if not data:
+			break
+		else:
+			if data == "close":
+				conn.close()
+			else:
+				conn.send(data)
+conn.close()
