e7f5e6266ae1d5ea5b194ba8553d5f689f43a155
mazafaka2026 <mazafaka2026@bk.ru>
2016-02-29 13:02:47 +0600

    Update test.py

--- test.py
+++ test.py
@@ -1,26 +1,33 @@
 print "helo"
 import socket
+
+class potok(threading.Thread):
+	def ClientTr():
+		print "test 1"
+		while True:
+ 			conn, addr = s.accept()
+			print ("conn="+ str(conn) )
+			print ("addr="+ str(addr) )
+			for x in xrange(20):
+				data=conn.recv(1024)
+				print ("data="+ str(data) )
+				if not data:
+					print ("data not="+ str(data) )
+					break
+				else:
+					if data == "close":
+						print ("data close="+ str(data) )
+						conn.close()
+					else:
+						print ("data else="+ str(data) )
+						conn.send(data)
+		conn.close()
+		
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('0.0.0.0', 2222))
 s.listen(10)
 #print ("s="+ str(s) )
 #conn, addr = s.accept()
 #print ("s=2="+ str(s) )
-while True:
-  conn, addr = s.accept()
-  print ("conn="+ str(conn) )
-  print ("addr="+ str(addr) )
-  for x in xrange(20):
-	 	data=conn.recv(1024)
-	 	print ("data="+ str(data) )
-	 	if not data:
-	 		print ("data not="+ str(data) )
-			break
-		else:
-			if data == "close":
-				print ("data close="+ str(data) )
-				conn.close()
-			else:
-				print ("data else="+ str(data) )
-				conn.send(data)
-conn.close()
+for x in xrange(10):
+ potok(target=potok.ClientTr).start()
