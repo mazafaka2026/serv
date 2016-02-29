f059f42fea8fca1ed1c42882845b7af1aa8688d2
mazafaka2026 <mazafaka2026@bk.ru>
2016-02-29 10:02:21 +0600

    Update test.py

--- test.py
+++ test.py
@@ -1,37 +1,40 @@
 print "helo"
 import socket, threading
-i=1
-class potok(threading.Thread):
-	def ClientTr():
-		print "test 1"
+#i=1
+#class potok(threading.Thread):
+#	def ClientTr():
+#		print "test 1"
 		
-		while True:
-			data = conn.recv(1024)
+#		while True:
+#			data = conn.recv(1024)
 
-			if not data:
-				break
+#			if not data:
+#				break
 
-			if data == "close":
-				conn.close()
-			else:
-				print ("data")
-				print(data)
-				print ("s")
-				print (s)
-				conn.send(data)
-				break
-		conn.close()
-		print("schetchik")
-		i=i+1
+#			if data == "close":
+#				conn.close()
+#			else:
+#				print ("data")
+#				print(data)
+#				print ("s")
+#				print (s)
+#				conn.send(data)
+#				break
+#		conn.close()
+#		print("schetchik")
+#		i=i+1
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('0.0.0.0', 2222))
 s.listen(10)
-while True:
-    conn, addr = s.accept()
-    print ("s")
-    print (s)
-    MAX_THREADS = 10
+conn, addr = s.accept()
+print ("s")
+print (s)
+#while True:
+#    conn, addr = s.accept()
+#    print ("s")
+#    print (s)
+#    MAX_THREADS = 10
    #A for x in xrange (10):
        # potok(target=potok.ClientTr,*args("")).start()
         #target=potok.ClientTr
