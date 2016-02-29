8d4de84babc64a07a0e6f50a805f008669ffe048
mazafaka2026 <mazafaka2026@bk.ru>
2016-02-29 08:57:46 +0600

    Update test.py

--- test.py
+++ test.py
@@ -1,32 +1,35 @@
 print "helo"
 import socket, threading
+i=1
+class potok(threading.Thread)
+	def ClientTr():
+		print "test 1"
+		while True:
+			data = conn.recv(1024)
 
-def ClientTr():
-    print "test 1"
-    while True:
-        data = conn.recv(1024)
-
-        if not data:
-            break
-        else:
-        	print ("data")
-        	print(data)
-		print ("s")
-		print (s)
-        	if data == "close":
-                	conn.close()
-        	else:
-                	conn.send(data)
-                	conn.close()
-
+			if not data:
+				break
+			else:
+				print ("data")
+				print(data)
+			print ("s")
+			print (s)
+				if data == "close":
+						conn.close()
+				else:
+						conn.send(data)
+						conn.close()
+						break
+		print("schetchik")
+		i=i+1
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('0.0.0.0', 2222))
-s.listen(1)
+s.listen(10)
 while True:
     conn, addr = s.accept()
-    if True:
-        threading.Thread(target=ClientTr).start()
+    if x in xrange(10):
+        potok().start()
         # threading.Thread(target=ClientTr).start()
         # threading.Thread(target=ClientTr).start()
         # threading.Thread(target=ClientTr).start()
